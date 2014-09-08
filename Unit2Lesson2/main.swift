a2057de1115fb7c6dbb1cdbcdf90c09dfea16d20
TapService Inc <git@tapservice-inc.com>
2014-09-07 20:08:11 -0400

    Initial Commit

---
+++ Unit2Lesson2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Unit2Lesson2
+//
+//  Created by Samuel Mbassa on 9/7/14.
+//  Copyright (c) 2014 Samuel Mbassa. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
